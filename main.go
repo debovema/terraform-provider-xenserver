cab77274daa90f3c97f1465e903c743ff5f96914
Maksym Borodin <maksym.borodin@gmx.de>
2018-01-17 13:09:55 +0100

    reference mborodin's version to test

--- main.go
+++ main.go
@@ -3,7 +3,7 @@ package main
 import (
 	"github.com/hashicorp/terraform/plugin"
 	"github.com/hashicorp/terraform/terraform"
-	"github.com/ringods/terraform-provider-xenserver/xenserver"
+	"github.com/mborodin/terraform-provider-xenserver/xenserver"
 )
 
 func main() {
